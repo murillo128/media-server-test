2efac76ff731c3aaf3c4631cedd73184849e0d83
stringa <sstringa@gmail.com>
2017-05-01 17:38:04 -0700

    posting app in a state where it defaults to broadcasting and it doesn't succeed.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, browserHistory, Link } from 'react-router';
 
 import './App.css';
 import RoomList from './pages/roomlist';
 import Broadcast from './pages/broadcast';
 
-import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import { Nav, Navbar, NavItem, Button, NavLink } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export default class App extends React.PureComponent {
 
     render() {
 
-        let component = this.props.children || <RoomList />;
+        let component = this.props.children || <Broadcast />;
 
         return (
           <div className="App">
 
-              <Navbar>
-                  <Navbar.Header>
-                      <Navbar.Brand>
-                          <a href="#">Medooze Media Server Example</a>
-                      </Navbar.Brand>
-                  </Navbar.Header>
-                  <Nav>
-                      <NavItem eventKey={1} href="/roomlist">Room List</NavItem>
-                      <NavItem eventKey={2} href="/broadcast">Broadcast</NavItem>
-                  </Nav>
-              </Navbar>
+              {/*<Navbar>*/}
+                  {/*<Navbar.Header>*/}
+                      {/*<Navbar.Brand>*/}
+                          {/*<a href="/">Medooze Media Server Example</a>*/}
+                      {/*</Navbar.Brand>*/}
+                  {/*</Navbar.Header>*/}
+                  {/*<Nav>*/}
+                      {/*/!*<LinkContainer to="/broadcast">*!/*/}
+                          {/*/!*<NavItem>Broadcast</NavItem>*!/*/}
+                      {/*/!*</LinkContainer>*!/*/}
+                      {/*/!*<LinkContainer to="/roomlist">*!/*/}
+                          {/*/!*<NavItem>RoomList</NavItem>*!/*/}
+                      {/*/!*</LinkContainer>*!/*/}
+                  {/*</Nav>*/}
+              {/*</Navbar>*/}
 
               {component}
 
@@ -36,12 +41,12 @@ export default class App extends React.PureComponent {
     }
 }
 
-ReactDOM.render(<Router history={browserHistory}>
-    <Route path='/' component={App}>
-        <Route path='broadcast' component={Broadcast}></Route>
-        <Route path='roomlist' component={RoomList}></Route>
-    </Route>
-</Router>, document.getElementById('root'));
+// ReactDOM.render(<Router history={browserHistory}>
+//     <Route path='/' component={App}>
+//         <Route path='broadcast' component={Broadcast}></Route>
+//         <Route path='roomlist' component={RoomList}></Route>
+//     </Route>
+// </Router>, document.getElementById('root'));
 
 
 
